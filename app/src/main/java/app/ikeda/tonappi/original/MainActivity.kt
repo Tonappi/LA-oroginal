0c520497d28b5952b879191a34c86c2d7ab82d6a
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-09-13 10:59:08 +0900

    MainActivityにViewBinding追加

--- app/src/main/java/app/ikeda/tonappi/original/MainActivity.kt
+++ app/src/main/java/app/ikeda/tonappi/original/MainActivity.kt
@@ -2,10 +2,14 @@ package app.ikeda.tonappi.original
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.ikeda.tonappi.original.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
