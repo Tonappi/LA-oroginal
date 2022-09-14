803dd89385737daad217f2f1649df61206ed25fd
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-09-14 09:15:15 +0900

    Dialogの修正

--- app/src/main/java/app/ikeda/tonappi/original/DatePicker.kt
+++ app/src/main/java/app/ikeda/tonappi/original/DatePicker.kt
@@ -12,19 +12,15 @@ import androidx.fragment.app.DialogFragment
 import java.util.*
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DatePicker.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class DatePicker : DialogFragment(), DatePickerDialog.OnDateSetListener {
     
     override fun onCreateDialog(savedInstanceState: Bundle?):Dialog {
 
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
+        val calender = Calendar.getInstance()
+        val year = calender.get(Calendar.YEAR)
+        val month = calender.get(Calendar.MONTH)
+        val day = calender.get(Calendar.DAY_OF_MONTH)
 
         //ActivityにDatePickerDialog.OnDateSetListenerがあればActivityに設定されている
         //onDateSet()を呼び出す
