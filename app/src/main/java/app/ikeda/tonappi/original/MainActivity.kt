e1ff5444887b57fe272bdd6b23e7ac164813e2eb
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-09-14 23:45:56 +0900

    DatePickerで選択した日にちを文字列として取得

--- app/src/main/java/app/ikeda/tonappi/original/MainActivity.kt
+++ app/src/main/java/app/ikeda/tonappi/original/MainActivity.kt
@@ -4,11 +4,12 @@ import android.app.DatePickerDialog
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.DatePicker
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import app.ikeda.tonappi.original.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.util.*
 
 //DataPickerDialog.OnDateSetListenerを追加
@@ -21,24 +22,31 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
-        /*/レンズ登録ボタンを押したとき
+        //レンズ登録ボタンを押したとき
         binding.lensAddButton.setOnClickListener {
             val lensList = arrayOf("ソフト(2週間)", "ハード(1ヶ月)")
             MaterialAlertDialogBuilder(this)
-            .setTitle("レンズの種類")
-            .setSingleChoiceItems(lensList, 0) { dialog, which ->
-                //選択されたラジオボタンの値を取得する
-                val kindOfLens: String= "ソフト(2週間)"
-            }
-            .setPositiveButton("はい") { dialog, which ->
-                //選択されたラジオボタンによって、変数casePeriodの値を変える
-                if ( kindOfLens = "ソフト(2週間)"){
-                    val LensPeriod :Int = 13
-                }else{
-                    val LensPeriod :Int = 30
-            }
-            .setNegativeButton("キャンセル", null)
-            .show() }
+                .setTitle("レンズの種類")
+                .setSingleChoiceItems(lensList, 0) { dialog, which ->
+                    //選択されたラジオボタンによって、変数lensPeriodの値を変える
+                    var kindOfLens: Int = which
+                    Log.d("レンズの種類",kindOfLens.toString())
+                    when (kindOfLens){
+                        0 -> {
+
+                        }
+                        1 -> {
+
+                        }
+                    }
+                }
+                .setPositiveButton("はい") { dialog, which ->
+
+                }
+                .setNegativeButton("キャンセル", null)
+                .show()
+                }
+
 
         //ケース登録ボタンを押したとき
         binding.caseAddButton.setOnClickListener {
@@ -47,34 +55,30 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
                 .setTitle("ケースの種類")
                 .setSingleChoiceItems(caseList, 0) { dialog, which ->
                     //選択されたラジオボタンによって、変数casePeriodの値を変える
-                    if ( kindOfLens = "ソフト(2週間)"){
-                        val LensPeriod :Int = 13
-                    }else{
-                        val LensPeriod :Int = 30}
+
                 }
-                .setPositiveButton("はい") {dialog, which ->
-                    // 値を保存する
-                    val kindsOfCase:String =
+                .setPositiveButton("はい") { dialog, which ->
+
                 }
                 .setNegativeButton("キャンセル", null)
-                .show() }*/
-
+                .show()
+        }
 
 
         //レンズ交換日を取得
         val lensEndyear = 2022
         var lensEndmonth = 9
-        val lensEnddate = 15
+        val lensEnddate = 17
 
         /*/ケース交換日を取得
         val caseEndyear =
-        val caseEndmonth
-        val caseEnddate*/
+        val caseEndmonth =
+        val caseEnddate =*/
 
         //現在の日時を取得
         val rightNow = Calendar.getInstance()
         Log.d("現在の日時", rightNow.toString())
-        lensEndmonth = lensEndmonth-1
+        lensEndmonth = lensEndmonth - 1
         rightNow.set(lensEndyear, lensEndmonth, lensEnddate);
         // 1970/1/1 から設定した rightNow のミリ秒
         val timeMillis1: Long = rightNow.getTimeInMillis()
@@ -83,39 +87,53 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
         //レンズ交換までの日数を計算
         // 差分のミリ秒
-        var diff = timeMillis1 - currentTimeMillis
+        var different = timeMillis1 - currentTimeMillis
         // ミリ秒から秒へ変換
-        diff = diff / 1000
+        different = different / 1000
         // minutes
-        diff = diff / 60
+        different = different / 60
         // hour
-        diff = diff / 60
+        different = different / 60
         // day
-        diff = diff / 24
+        different = different / 24
         //レンズ交換までの日数を表示
-         val lensCountdown: String = "$diff 日"
+        val lensCountdown: String = "$different 日"
         //ケース交換までの日数を計算
         binding.lensDayleftView.text = lensCountdown
 
+        //レンズの日付登録ボタンクリック時
+        binding.lensDayButton.setOnClickListener {
+            showDatePickerDialog()
+            //binding.lensPeriodView.text = "$StartDate~"
+            //val LensStartDate =
+        }
+
+        //ケースの日付登録ボタンクリック時
+        binding.caseDayButton.setOnClickListener {
+            showDatePickerDialog()
+            //binding.casePeriodView.text = "$StartDate~"
+            //val CaseStartDate =
+        }
 
     }
 
     //DatePickerDialogを呼び出すメソッド
     override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int) {
-        //レンズの使用開始日を表示
-            val lensStartDate: String = getString(R.string.lensstringformat, year, monthOfYear+1, dayOfMonth)
-            binding.lensPeriodView.text = "$lensStartDate~"
-        //日付を取得
+        //使用開始日を文字列で取得
+        val StartDate: String =getString(R.string.stringformat, year, monthOfYear + 1, dayOfMonth)
+        Log.d("開始日",StartDate.toString())
 
     }
 
-    private fun showDatePickerDialog(v: View) {
+    private fun showDatePickerDialog() {
         val newFragment = DatePicker()
         newFragment.show(supportFragmentManager, "datePicker")
     }
 
-    fun onDataset(view: View) {}
-
 }
+
+
+
+
 
 
